e7e3ba064c1de31ed245d798b104394ccbc735ba
Dmitry Stoyanovsky <dmitry.stoyanovsky@banuba.com>
2021-07-05 17:44:42 +0300

    fix copy phase MSVC

--- cmake/copy_libs.cmake
+++ cmake/copy_libs.cmake
@@ -1,7 +1,7 @@
 function(copy_sdk target)
     if (MSVC)
         set_property(TARGET ${target} APPEND PROPERTY LINK_FLAGS /STACK:4194304)
-        set(SDK_EFFECT_PLAYER_LIB "BanubaEffectPlayerC")
+        set(SDK_EFFECT_PLAYER_LIB "BNBEffectPlayerC")
         set(SDK_FILE_TYPE "dll")
 
         add_custom_command(
